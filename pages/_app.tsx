df1469306500d1fbf97e06e6391e3a29ae034a3d
Nghia Mai <nghia.mai@foodpanda.com>
2022-06-05 01:40:37 -0400

    add dropzone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+
+import { FunctionComponent, PropsWithChildren } from 'react';
+
+import type { AppProps } from "next/app"
+
+const SafeHydrate: FunctionComponent<PropsWithChildren<unknown>> = ({
+  children,
+}) => {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  )
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SafeHydrate>
+      <Component {...pageProps} />
+    </SafeHydrate>
+  )
 }
 
 export default MyApp
